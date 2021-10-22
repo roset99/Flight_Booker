91284d240ae4d83b02da1fd27d18e8b7882c5725
Nagina Rafique <CORP\nagina.rafique@a-be2mw3687wnp.corp.amazonworkspaces.com>
2021-10-22 18:50:39 +0100

    added viewflight

--- flight_booking/src/main/java/com/rose/flightbookings/Main.java
+++ flight_booking/src/main/java/com/rose/flightbookings/Main.java
@@ -1,16 +1,22 @@
 package com.rose.flightbookings;
 
 import java.awt.print.Book;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         BookingSystem bookingSystem = new BookingSystem(2,2);
 //        NewPassenger john = new NewPassenger(bookingSystem);
 //        john.newPassenger();
-        System.out.println(bookingSystem.toString());
-        AddFlight toEgypt = new AddFlight(bookingSystem);
-        toEgypt.addFlight();
-        toEgypt.addFlight();
+//        System.out.println(bookingSystem.toString());
+//        AddFlight toEgypt = new AddFlight(bookingSystem);
+//        toEgypt.addFlight();
+//        toEgypt.addFlight();
+
+        Passenger passengerFlight = new Passenger("Jane", "Doe", "777777", LocalDate.of(2008, 10, 23), Citizenship.BRITISH);
+
+        ViewFlight menu= new ViewFlight(bookingSystem, passengerFlight);
+        menu.flightMenu();
 
     }
 }
